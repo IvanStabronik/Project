8a47f180e86b4b30925697429d82c06e4dd4bc65
Ivan <st.jn.game@gmail.com>
2024-08-06 10:43:00 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+/*const carBodyWidth = 5000,
+    carBodyLength = 4000;
+
+console.log("Ширина: " + vehicleBodyWidth + ", длина: " + vehicleBodyLength);
+
+["sadsa", "sdasd", "sdasd"].map(a =>)*/
+
+//Request
+// data
+// response
+
+
+//snake_case
+//UPPER_SNAKE_CASE
+//Kebab-case
+//PascalCase
+
+/*const COLOR_RED = '#F00';
+
+const _apiBase = 'sadasdas'*/
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
